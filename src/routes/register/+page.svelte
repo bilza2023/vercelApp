6a53c01eb134a29cd68f7d91c39d6bddb9de4727
unknown <bilza32611@gmail.com>
2023-07-19 02:16:03 +0500

    in the name of Fire

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -14,7 +14,7 @@ import {PageWrapper,HdgWithIcon,Centre,SectionHead,FormRow,InputElm,NumberElm,Co
     <div class='w-10/12 '>
         <FormElm>
             <div class='flex justify-center'>
-                    <HdgWithIcon icon={Icons.KEY} >Login</HdgWithIcon>
+                    <HdgWithIcon icon={Icons.KEY} >Register</HdgWithIcon>
             </div>
 
             <div class='flex justify-center pt-1 text-white'>
@@ -38,8 +38,19 @@ import {PageWrapper,HdgWithIcon,Centre,SectionHead,FormRow,InputElm,NumberElm,Co
                 </div>
             </div>
            
+            <div class='flex justify-center pt-1 text-white'>
+            Confirm Password
+            </div>
+
+            <div class='flex justify-center pt-1'>
+                <div class='w-6/12 text-center'>
+                    <InputElm value={""} />
+                </div>
+            </div>
+
+
             <div class='flex justify-center'>
-            <BtnWIconSm icon={Icons.SAVE} title='Login' />
+            <BtnWIconSm icon={Icons.SAVE} title='Register' />
             </div>
         </FormElm>
     </div>
