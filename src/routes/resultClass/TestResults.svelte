5c495cd1e335777e06a695695e9a243675d6c632
unknown <bilza32611@gmail.com>
2023-08-06 10:20:15 +0500

    in the name of Allah

--- src/routes/resultClass/TestResults.svelte
+++ src/routes/resultClass/TestResults.svelte
@@ -1,18 +1,35 @@
 <script>
-//@ts-nocheck
-export let results;
+  //@ts-nocheck
+  export let results;
 </script>
 
+<div class='flex justify-center '>
+    <table class='w-full border-separate'>
+      <tr class='border-separate'>
+        <!-- Apply the custom-td class to all td elements -->
+        <td class='custom-td'>Student Id</td>
+        <td class='custom-td'>Student Name</td>
+        <td class='custom-td'>Percentage</td>
+      </tr>
 {#each results as result }
-<div class='flex  gap-1'>
-    <p class='p-1 m-1 bg-gray-600 rounded-md'>Student Id : {result.studentId}</p>
-    <p class='p-1 m-1 bg-gray-600 rounded-md'>Student Name : {result.studentName}</p>
-    <p class='p-1 m-1 bg-gray-600 rounded-md'>Marks Obtained : 33.44</p>
-</div>
-
-
-
+  
+      <tr class='border-separate'>
+        <!-- Apply the custom-td class to all td elements -->
+        <td class='custom-td'>{result.studentId}</td>
+        <td class='custom-td'>{result.studentName}</td>
+        <td class='custom-td'>{(Math.random() * 100).toFixed(2)} %</td>
+      </tr>
 {/each}
+    </table>
+  </div>
 
-
-
+<style>
+  /* Define a new CSS class with Tailwind CSS classes */
+  .custom-td {
+    /* Use Tailwind CSS utility classes here */
+    @apply p-1 m-1 border-2 border-gray-600 text-center;
+    /* Add more Tailwind CSS classes as needed */
+    /* For example: */
+    /* @apply bg-gray-100 text-gray-900; */
+  }
+</style>
