9aaa01509a48cf330a7103e14ee887e2b3fdcc2f
unknown <bilza32611@gmail.com>
2023-08-16 01:42:19 +0500

    testEdit complete

--- src/routes/editTest/quizObj/QuizObj.js
+++ src/routes/editTest/quizObj/QuizObj.js
@@ -1,131 +1,149 @@
 //@ts-nocheck
 //EditQuizObj is a wrapper around itemStore.and also contains questionObj
-import { itemStore ,questionsStore,visibleDialogStore} from '../store.js';
+import { itemStore, questionsStore, visibleDialogStore } from '../store.js';
 import QuestionsObj from './QuestionsObj.js';
-import {get,Agent,goto,toast} from "$lib/util";
-import quizStringify from './quizStringify.js';
+import { get, Agent, goto, toast } from "$lib/util";
 
-export default class QuizObj  {
+export default class QuizObj {
 
-    constructor(){
+    constructor() {
         this.questions = new QuestionsObj();
     }
 
     set(value) {
-        itemStore.set(value);    
+        itemStore.set(value);
     }
 
-    async delete(title){
-        try{
-            // debugger;
-            const item = get(itemStore); 
-            if (title !== item.title){
+    async delete(title) {
+        try {
+            //debugger;
+            const item = get(itemStore);
+            if (title !== item.title) {
                 toast.push('Title does not match');
                 return;
             }
 
-            const resp = await Agent.del('test',{ id : item._id });
+            const resp = await Agent.del('test', { id: item._id });
 
-            if (resp.ok == true){
+            if (resp.ok == true) {
                 visibleDialogStore.set('');
                 goto("/tests");
                 toast.push('deleted');
-            }else {
+            } else {
                 const data = await resp.json();
                 toast.push(data.message);
             }
-        }catch(e){
-            console.log( e );
+        } catch (e) {
+            console.log(e);
         }
     }
 
-    async clone(title){
-        try{
-      //   debugger;
-            //--getDeepCopy is local fn and alsocombines quiz with Qs
-            let  item = getDeepCopy();
-            item  = await quizStringify(item);
+    async clone(title) {
+        try {
+            //debugger;
 
-            const resp = await Agent.create('test' , {title});
+            let item = combQnQ();
+
+            const resp = await Agent.create('test', { title });
             if (resp.ok) {
-                const data  = await resp.json();
+                const data = await resp.json();
                 const newItem = data.item;
                 item._id = newItem._id;
                 item.title = title;
+                item.classId = ''; //dont save classId
+                //--here stringify
+                item = await quizStringify(item);
 
-                const resp2 = await Agent.update('test',{ item });
+                const resp2 = await Agent.update('test', { item });
 
-                if (resp2.ok){
-                    toast.push( "Cloned" );
+                if (resp2.ok) {
+                    toast.push("Cloned");
                     visibleDialogStore.set('');
-                }else {
-                    toast.push( "failed to clone" );        
+                } else {
+                    toast.push("failed to clone");
                 }
-            }else {
-                toast.push( "failed to clone" );        
+            } else {
+                toast.push("failed to clone");
             }
-        }catch(e){
-            console.log( e );
+        } catch (e) {
+            console.log(e);
         }
     }
-    async run(){
-       try{
+    async run() {
+        try {
+            //debugger;
+            // getDeepCopy has combined the quiz and questions and in the process the questions.content has also become strignified. BUT it is ok since we have to use quiz.title/classId/private etc and we are not using quiz.questions so they are ok for DB just do not re-stringify them.
+            let item = combQnQ();
+            // ////////////========== Test
+            if (item.title === '') {
+                toast.push('Missing title');
+                return;
+            }
+            // ////////////========== Test
+            if (item.private === true) { // if private only then needs classId
+                if (item.classId === '' || item.classId === null) {
+                    toast.push('Assign a Class to the test');
+                    return;
+                }
+            }
+            item.testId = item._id;
+            item._id = null;
+            // item.title = title; //-use later to set new title
+            item.published = true; //changed ??????
+            item.createdAt = new Date();
+            //////////////////////////////////////
+            //--here stringify
+            item = await quizStringify(item);
+            //////////////////////////////////////
+            const resp = await Agent.create('run', { item });
+            if (resp.ok) {
+                visibleDialogStore.set('');
+                toast.push("Test is Running now");
+            } else {
+                toast.push("Failed to Run test");
+            }
 
-     let  item = getDeepCopy();
-    // ////////////========== Test
-    if (item.title === ''){
-        toast.push('Missing title');
-        return;
-    }    
-      // debugger;
-   // ////////////========== Test
-   if (item.private === true){ // if private only then needs classId
-   if (item.classId  === '' || item.classId  === null ){
-      toast.push('Assign a Class to the test');
-      return;
-   }    
-   }
-    
-// console.log("item before Run" , item);    
-    item.testId = item._id;
-    item._id = null;
-    // item.title = title; //-use later to set new title
-    item.published = true; //changed ??????
-    item.createdAt =  new Date();
-    //////////////////////////////////////
-    // last step stringify questions ---stringify here not at top 
-    item  = await quizStringify(item);
-        const resp = await Agent.create('run',{item});
-        if (resp.ok) {
-            // const data = await resp.json();
-            visibleDialogStore.set('');
-            toast.push( "Test is Running now" );
-        }else {
-            toast.push( "Failed to Run test" );
+        } catch (e) {
+            toast.push(e.message);
         }
-    
-    }catch(e){
-        toast.push(e.message);
     }
+    async save() {
+        try {
+            // debugger;
+            let item = combQnQ();
+            // item.classId = ''; since the test does not have a class id just the "run" has it. This will help us to keep the classId  = none and we will not save new runs with faluty classId. it needs to be set every time.
+            item.classId = '';
+            item = await quizStringify(item);
+
+            const resp = await Agent.update('test', { item });
+
+            if (resp.ok) {
+                const data = await resp.json();
+                toast.push("saved");
+            } else {
+                const data = await resp.json();
+                toast.push(data.message);
+            }
+
+        } catch (e) {
+            toast.push('Failed to save');
+        }
     }
 }//class obj ends
-
-///////////////////////////////////////////////////
-async function getDeepCopy(){
-        ///XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX////
-        //--- get deep copy of Quiz
-        let quiz = await JSON.parse(JSON.stringify(  get(itemStore) ));
-        //--- get deep copy of Questions
-        ///XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX////
-        let questions = await JSON.stringify(  get(questionsStore) );
-        //--- combine both 
-        quiz.questions = questions;
-        return quiz;
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+function combQnQ() {
+    let quiz = get(itemStore);
+    let questions = get(questionsStore);
+    quiz.questions = questions;
+    return quiz;
 }
+async function quizStringify (item){
+ 
+ for (let i = 0; i < item.questions.length; i++) {
+    item.questions[i].content = JSON.stringify(item.questions[i].content);
+  }
+//  item.questions = questions;
+ return item;
 
-function combQnQ(){
-let quiz =   get(itemStore);
-let questions = get(questionsStore); 
-quiz.questions = questions;
-return quiz;
 }
