90acbabcbdb6e10e9cfcd09a3da0dfaa272195a9
unknown <bilza32611@gmail.com>
2023-08-29 12:03:47 +0500

    starting

---
+++ src/routes/editQuestions/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+// @ts-nocheck
+import {PageWrapper,HdgWithIcon,Centre,Loading,SectionHeadIcon} from '$lib/cmp';
+import {onMount,toast,fade,Agent} from '$lib/util';
+
+import Toolbar from './Toolbar.svelte';
+
+let question = '';
+onMount(async ()=>{
+  try{
+  // debugger;
+    // const quizId = new URLSearchParams(location.search).get("quizId");
+    // const resp = await Agent.readone('test' , {id: quizId });
+    // if (resp.ok){
+    //   const data = await resp.json();
+    //   question = new QuizObj(data.item);
+    // }else {
+        // toast.push('failed to load');
+    // }
+  } catch (e) {
+       toast.push('failed to load');
+  }      
+});
+
+import MainNav from '$lib/appComp/MainNav.svelte';
+/////////////////////////////////////////////////////////////////
+</script>
+  <!-- ****************************************** -->
+<MainNav/>
+<PageWrapper>
+
+<Toolbar  {question}/>
+
+        <!-- ************** -->
+        <!-- THE MAIN CODE ENDS -->
+        <Centre>
+        <HdgWithIcon icon='📜'>{'Banana'}</HdgWithIcon>
+        </Centre>
+
+     
+<!-- <Loading /> -->
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</PageWrapper>
---
+++ src/routes/editQuestions/Toolbar.svelte
@@ -0,0 +1,43 @@
+<script>
+  // @ts-nocheck
+  import { PageNav, PageNavBtn } from '$lib/cmp';
+  import { Icons } from '$lib/util';
+
+
+</script>
+
+<PageNav>
+  <PageNavBtn
+    icon={Icons.SAVE}
+    title="Save"
+    tf={true}
+    trueColor={"text-white"}
+    clk={() =>console.log('ok')}
+  />
+
+  <PageNavBtn
+    icon={Icons.RUN}
+    title="Run"
+    tf={true}
+    trueColor={"text-white"}
+    clk={() =>console.log('ok')}
+  />
+
+  <PageNavBtn
+    icon={"🐑"}
+    title="Clone"
+    tf={false}
+    trueColor={"text-white"}
+    falseColor={"text-gray-600"}
+    clk={() =>console.log('ok')}
+  />
+
+  <PageNavBtn
+    icon={"🗑️"}
+    title="Delete"
+    tf={false}
+    trueColor={"text-white"}
+    falseColor={"text-gray-600"}
+    clk={() =>console.log('ok')}
+  />
+</PageNav>
