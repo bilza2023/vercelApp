1d9a90bb74c51ba63fdaffec8f29e0135e7b7d68
unknown <bilza32611@gmail.com>
2023-10-28 23:38:57 +0500

    great strides made in grid-player but thats all for now  lets merge with master and continue again tomarow

---
+++ src/routes/grid/Cell.svelte
@@ -0,0 +1,48 @@
+
+<script>
+//@ts-nocheck
+import {runningTime,isPlayingStore} from "./store";
+
+$:isPlaying = $isPlayingStore;
+$:rTime = $runningTime;
+export let global;
+export let cell;
+
+$:{
+        rTime;
+        console.log("runningTime -from-cell",rTime);
+        cell.color = getColor(cell.startTime); 
+
+}
+function getColor(startTime) {
+    // debugger;
+   if (!isPlaying ){
+                // console.log("isPlaying",isPlaying);
+         return global.cellFontColor;
+   }else {
+          if (startTime <=  rTime) {
+                // console.log("global.cellFontColor",global.cellFontColor);
+               return global.cellFontColor;
+         }else {
+                // console.log("global.bgColor",global.bgColor);
+            return global.bgColor; 
+         }
+   }
+}
+</script>
+
+ <div class=  "hover:cursor-pointer   border-2  text-center flex items-center justify-center" 
+  style= {`
+            min-width: 70px;
+            min-height: 70px;
+            font-size : ${global.fontSize}em;
+            margin : ${global.margin}px;
+            padding : ${global.padding}px;
+            color : ${cell.color};
+        `}
+ 
+ >
+{cell.content},
+{cell.color}
+
+</div>
--- src/routes/grid/demo.js
+++ src/routes/grid/demo.js
@@ -5,25 +5,18 @@ return {
 bgColor : "#293749",
 fontSize : 1,
 rows : [
-
-            {
-            startTime : 0,
-            endTime : 100 ,
-            cells : [
+    // one row is an array of cell objects
+         [
                     {content : "2^4" ,startTime : 2,endTime : 0 , bt : true,bl:false,br:true,bb:false , bt : true,bl:false,br:true,bb:false},
                     {content : "\\frac{3}{4}",startTime : 4,endTime : 0 , bt : true,bl:false,br:true,bb:false},
                     {content : "\\sqrt{345345}",startTime : 6,endTime : 0 , bt : true,bl:false,br:true,bb:false},
-                ]
-            },
-            {
-            startTime : 0,
-            endTime : 100 ,
-            cells : [
+        ],
+          
+        [
                     {content : "\\frac{3}{\\sqrt{333}}" ,startTime : 8,endTime : 0 , bt : true,bl:false,br:true,bb:false , bt : true,bl:false,br:true,bb:false},
                     {content : "\\frac{44}{88}",startTime : 10,endTime : 0 , bt : true,bl:false,br:true,bb:false},
                     {content : "\\sqrt{555}",startTime : 12,endTime : 0 , bt : true,bl:false,br:true,bb:false},
-                ]
-            },
+        ]
             
 ]
 
