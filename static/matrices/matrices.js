a44d9ddce2c77fd66eb74e18ae1609b60f6303ec
unknown <bilza32611@gmail.com>
2024-01-16 05:24:54 +0500

    great-matrix introduction

--- static/matrices/matrices.js
+++ static/matrices/matrices.js
@@ -15,7 +15,7 @@ export const syllabus = [
     "slides": [
       {
         "startTime": 0,
-        "endTime": 10,
+        "endTime": 20,
         "type": "HdgList",
         "version": "0.0.0",
         "template": "",
@@ -33,6 +33,149 @@ export const syllabus = [
           }
         ],
         "_id": "65a46075f8c94540ac203bf5"
+      },
+      {
+        "startTime": 20,
+        "endTime": 95,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "Introduction to Matrices",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a54a5b2790340925f0ce50"
+          },
+          {
+            "name": "para",
+            "content": "Matrices are one of the most useful and versatile tools in mathematics. They can be used to organize, store, manipulate, and analyze large amounts of data. They can also be used to model and solve various problems in science, engineering, business, and many other fields.\n",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a54a5b2790340925f0ce51"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65a54a5b2790340925f0ce4f"
+      },
+      {
+        "startTime": 95,
+        "endTime": 115,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "What are matrices?",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a55032e1ea86e4080049e9"
+          },
+          {
+            "name": "para",
+            "content": "A matrix is a rectangular array of numbers, arranged in rows and columns. Each number in a matrix is called an element or an entry. We can identify an element by its row and column position.",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a55032e1ea86e4080049ea"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65a55032e1ea86e4080049e8"
+      },
+      {
+        "startTime": 115,
+        "endTime": 198,
+        "type": "img",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "",
+            "content": "matrix_rows_columns",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb51"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65a5c2bfeb5ee461d23edb50"
+      },
+      {
+        "startTime": 198,
+        "endTime": 273,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "Why we use Matrices",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb53"
+          },
+          {
+            "name": "para",
+            "content": "Matrices can represent and manipulate data in a compact and convenient way. For example, we can use a matrix to store a table of values, such as grades, prices, distances, etc. We can also use a matrix to store an image, where each element represents the color or intensity of a pixel.",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb54"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65a5c2bfeb5ee461d23edb52"
+      },
+      {
+        "startTime": 273,
+        "endTime": 309,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "Why we use Matrices",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb56"
+          },
+          {
+            "name": "para",
+            "content": "Matrices can perform operations on data efficiently and easily. For example, we can use matrices to add, subtract, multiply, or divide data by applying simple rules. We can also use matrices to transform data, such as rotating, scaling, or reflecting an image.",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb57"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65a5c2bfeb5ee461d23edb55"
+      },
+      {
+        "startTime": 309,
+        "endTime": 382,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "Why we use Matrices",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb59"
+          },
+          {
+            "name": "para",
+            "content": "- Matrices can model and solve systems of linear equations. For example, we can use a matrix to represent the coefficients and constants of a system of equations. \n- Matrices can encode and decode information. For example, we can use matrices to encrypt or decrypt messages, by using certain rules to change the elements of a matrix.\n- We can also use matrices to compress or decompress data, by using certain techniques to reduce the size or increase the quality of a matrix.",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65a5c2bfeb5ee461d23edb5a"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65a5c2bfeb5ee461d23edb58"
       }
     ],
     "teacherComments": "",
