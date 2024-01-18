5d29856658327e5ff0bc2d2d31aed0757b5cb95a
unknown <bilza32611@gmail.com>
2024-01-18 23:18:32 +0500

    in the name of Allah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<svelte:head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4279476755172786"
+     crossorigin="anonymous"></script>
+</svelte:head>
+
+
 <script>
 //@ts-nocheck
 import {PageWrapper,HdgWithIcon,Centre} from '$lib/cmp';
