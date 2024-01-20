bfb14555ab19bf38faae3c44d1d70e035190ddc0
unknown <bilza32611@gmail.com>
2024-01-21 02:26:00 +0500

    matrix inversion formula

--- static/matrices/matrices.js
+++ static/matrices/matrices.js
@@ -183,46 +183,6 @@ export const syllabus = [
     "__v": 0
   },
   {
-    "_id": "65a4604ef8c94540ac203bed",
-    "chapter": 1,
-    "exercise": "1",
-    "questionNo": 2,
-    "part": 0,
-    "name": "matrix_order",
-    "sortOrder": 0,
-    "questionType": "paid",
-    "status": "final",
-    "free": false,
-    "filename": "matrices_ch_1_ex_1_q_2_pt_0_matrix_order",
-    "filledBy": "",
-    "slides": [
-      {
-        "startTime": 0,
-        "endTime": 10,
-        "type": "HdgList",
-        "version": "0.0.0",
-        "template": "",
-        "items": [],
-        "slideExtra": [
-          {
-            "key": "data",
-            "value": "Matrix Order",
-            "_id": "65a4608cf8c94540ac203bfe"
-          },
-          {
-            "key": "rows",
-            "value": "2",
-            "_id": "65a4608cf8c94540ac203bff"
-          }
-        ],
-        "_id": "65a4608cf8c94540ac203bfd"
-      }
-    ],
-    "teacherComments": "",
-    "adminComments": "",
-    "__v": 0
-  },
-  {
     "_id": "65aa9f91d403c62292b316bd",
     "chapter": 1,
     "exercise": "1",
@@ -346,6 +306,365 @@ export const syllabus = [
         ],
         "slideExtra": [],
         "_id": "65a2744d08f47d00c9267a49"
+      }
+    ],
+    "teacherComments": "",
+    "adminComments": "",
+    "__v": 0
+  },
+  {
+    "_id": "65ac2a5ce9934ce11c2d5391",
+    "chapter": 1,
+    "exercise": "1",
+    "questionNo": 1,
+    "part": 0,
+    "name": "Matrix_Inversion_Method",
+    "sortOrder": 0,
+    "questionType": "paid",
+    "status": "final",
+    "free": false,
+    "filename": "matrices_ch_1_ex_1_q_1_pt_0_Matrix_Inversion_Method",
+    "filledBy": "",
+    "slides": [
+      {
+        "startTime": 0,
+        "endTime": 150,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "What is  Matrix Inversion Method",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87471e"
+          },
+          {
+            "name": "para",
+            "content": "This method is used to solve a system of linear equations that can be represented in the form Ax = B :where \n    - A is the matrix of coefficients\n    - x is the column matrix of variables\n    - B is the column matrix of solutions\nThis method can be applied only when the coefficient matrix is a square matrix and non-singular.",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87471f"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65ac3595fdc86acd9d87471d"
+      },
+      {
+        "startTime": 150,
+        "endTime": 173,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "What is a Square Matrix",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874721"
+          },
+          {
+            "name": "para",
+            "content": "A Square Matrix is a matrix that has the same number of rows and columns. For example, if a matrix has n rows and n columns, it is called a square matrix of order n",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874722"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65ac3595fdc86acd9d874720"
+      },
+      {
+        "startTime": 173,
+        "endTime": 218,
+        "type": "HdgPara",
+        "version": "0.0.0",
+        "template": "",
+        "items": [
+          {
+            "name": "heading",
+            "content": "What is Non-Singular Matrix",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874724"
+          },
+          {
+            "name": "para",
+            "content": "A Non-Singular Matrix is a square matrix that has an inverse, meaning its determinant is not zero.\nWe have to find determinant of Matrix A first to find out that even if the solution is possible or not.",
+            "showAt": 0,
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874725"
+          }
+        ],
+        "slideExtra": [],
+        "_id": "65ac3595fdc86acd9d874723"
+      },
+      {
+        "startTime": 218,
+        "endTime": 425,
+        "type": "Eqs",
+        "version": "",
+        "template": "",
+        "items": [
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 0,
+              "startTime": 218,
+              "endTime": 40,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "The Matrix Inversion Formula",
+              "type": "hdg",
+              "sp": [],
+              "fs": [],
+              "fsVisibility": false,
+              "spVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874727"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 1,
+              "startTime": 221,
+              "endTime": 50,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "x = A^{-1}B",
+              "type": "code",
+              "sp": [],
+              "fs": [],
+              "fsVisibility": false,
+              "spVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874728"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 2,
+              "startTime": 238,
+              "endTime": 60,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "Convert a System of Linear Equations into Matrix Form",
+              "type": "hdg",
+              "sp": [],
+              "fs": [],
+              "fsVisibility": false,
+              "spVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874729"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 3,
+              "startTime": 240,
+              "endTime": 70,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "\\begin{matrix} 2x-2y=4 \\\\ 3x+2y=6\\  \\end{matrix}",
+              "type": "code",
+              "sp": [],
+              "fs": [],
+              "fsVisibility": false,
+              "spVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87472a"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 4,
+              "startTime": 253,
+              "endTime": 80,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "We can also write it as ",
+              "type": "hdg",
+              "sp": [],
+              "fs": [],
+              "fsVisibility": false,
+              "spVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87472b"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 5,
+              "startTime": 255,
+              "endTime": 90,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "A=\\begin{bmatrix} \n2 & -2\n\\\\\n 3 &  2\n\\end{bmatrix}",
+              "type": "code",
+              "sp": [
+                {
+                  "code": "Coefficients Matrix",
+                  "type": "heading"
+                },
+                {
+                  "code": "A is called Coefficients Matrix",
+                  "type": "text"
+                }
+              ],
+              "fs": [],
+              "spVisibility": false,
+              "fsVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87472c"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 6,
+              "startTime": 275,
+              "endTime": 100,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "X= \\begin{bmatrix} \nx\n\\\\\n y\n\\end{bmatrix} ",
+              "type": "code",
+              "sp": [
+                {
+                  "code": "Variables Matrix",
+                  "type": "heading"
+                }
+              ],
+              "fs": [],
+              "spVisibility": false,
+              "fsVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87472d"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 7,
+              "startTime": 281,
+              "endTime": 110,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "B= \\begin{bmatrix} \n4\n\\\\\n 6\n\\end{bmatrix} ",
+              "type": "code",
+              "sp": [
+                {
+                  "code": "Solutions Matrix",
+                  "type": "heading"
+                }
+              ],
+              "fs": [],
+              "spVisibility": false,
+              "fsVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87472e"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 8,
+              "startTime": 288,
+              "endTime": 120,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "A * X = B",
+              "type": "code",
+              "sp": [
+                {
+                  "code": "We can write it in this format since if we multiply the A and X we get back the same equations that we started with",
+                  "type": "text"
+                }
+              ],
+              "fs": [],
+              "spVisibility": false,
+              "fsVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d87472f"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 9,
+              "startTime": 327,
+              "endTime": 130,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "X = A^{-1} B",
+              "type": "code",
+              "sp": [
+                {
+                  "code": "Matrix X has the values of x and y",
+                  "type": "heading"
+                }
+              ],
+              "fs": [],
+              "spVisibility": false,
+              "fsVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874730"
+          },
+          {
+            "name": "",
+            "content": "",
+            "showAt": 0,
+            "extra": {
+              "step": 10,
+              "startTime": 376,
+              "endTime": 140,
+              "fsStartTime": 0,
+              "fsEndTime": 0,
+              "code": "A^{-1} = AdjA/ Determinant - A",
+              "type": "code",
+              "sp": [
+                {
+                  "code": "We get it by dividing Adjoint of A by determinant of A",
+                  "type": "text"
+                }
+              ],
+              "fs": [],
+              "spVisibility": false,
+              "fsVisibility": false
+            },
+            "arr": [],
+            "_id": "65ac3595fdc86acd9d874731"
+          }
+        ],
+        "_id": "65ac3595fdc86acd9d874726",
+        "slideExtra": []
       }
     ],
     "teacherComments": "",
