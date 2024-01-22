1a6a5079c459cbac8f62fd4927edff3a6f59c991
unknown <bilza32611@gmail.com>
2024-01-22 16:39:43 +0500

    image tall and wide now

--- src/lib/Presentation/slides/img/Img.svelte
+++ src/lib/Presentation/slides/img/Img.svelte
@@ -8,18 +8,21 @@ export let theme;
 // primaryColor, secondaryColor, backgroundColor, textColor, highlightColor
 </script>
 
-<div class="flex flex-col justify-start min-h-screen"
+<div class="flex flex-col justify-start min-h-screen w-full"
      style={`
  background-color: ${theme.backgroundColor};
  `}
 >
 
-  <div class="flex justify-center w-full p-2 pb-4">
-    <img style="max-height: 75vh; width: auto; max-width: 90%;" src={`./${tcode}/images/${items[0].content}.png`} alt="">
+  <div class="flex justify-center w-full  pb-4">
+    <img style="max-height: 85vh;  width: 100%;" src={`./${tcode}/images/${items[0].content}.png`} alt="">
   </div>
 
   <div class="flex justify-end w-full pr-10">
     <div style={`color: ${theme.highlightColor}`}>taleem.help</div>
   </div>
 
+<br>
+<br>
+<br>
 </div>
